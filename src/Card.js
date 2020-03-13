2a0dc5686544a4c3f403fee0d5618baf87fb5f7c
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-03-13 14:29:50 +0100

    Cleaned autogenerated code

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Card() {
+    return(
+        <div>
+            <img alt='robot' src=''/>
+            <div>
+                <h2>Name</h2>
+                <p>Address</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
