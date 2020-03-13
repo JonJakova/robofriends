3521feff265eab620681ffaf99179585d313e218
JonJakova <50449118+JonJakova@users.noreply.github.com>
2020-03-13 16:20:30 +0100

    test cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Card() {
+function Card({id, name, email}) {
     return(
-        <div>
-            <img alt='robot' src=''/>
+        <div className='tc bg-light-green dib br3 pa3 ma2 bw2 shadowd-5'>
+            <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>Name</h2>
-                <p>Address</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
